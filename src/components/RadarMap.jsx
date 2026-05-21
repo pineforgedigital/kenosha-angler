57028223674f3c86f3bb8bb0b848fa89f8d73bb2
Pine Forge Digital <pineforgedigital@gmail.com>
2026-05-20 21:57:25 -0500

    fix: load mapbox-gl.css from CDN to prevent missing CSS error

--- src/components/RadarMap.jsx
+++ src/components/RadarMap.jsx
@@ -1,6 +1,5 @@
 import React, { useRef, useEffect, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
-import 'mapbox-gl/dist/mapbox-gl.css';
 import { Radar } from 'lucide-react';
 
 export default function RadarMap({ activeLocation, squallAlert }) {
